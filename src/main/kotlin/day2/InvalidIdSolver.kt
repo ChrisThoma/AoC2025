7aa00bb1a4e47199665678c685bd3709df2020cf
chris thoma <christhoma0@gmail.com>
2025-12-02 15:20:50 -0500

    Solves day 2 part 2.

--- src/main/kotlin/day2/InvalidIdSolver.kt
+++ src/main/kotlin/day2/InvalidIdSolver.kt
@@ -41,10 +41,44 @@ object InvalidIdSolver : ProblemRunner {
     }
 
     override fun answerPartTwo(input: File): String {
-        TODO("Not yet implemented")
+        val scanner = Scanner(input)
+        val ranges = scanner.nextLine().split(',')
+        var total: Long = 0L
+        for (range in ranges) {
+            val pair = range.split('-')
+            val low = pair[0]
+            val high = pair[1]
+            val distance = high.toLong() - low.toLong()
+            for (i in 0..distance) {
+                val current = low.toLong() + i
+                val currentString = current.toString()
+                if (isRepeatedPattern(currentString)) {
+                    total += current
+                }
+            }
+        }
+        return "$total"
     }
+
+    private fun isRepeatedPattern(s: String): Boolean {
+        val n = s.length
+
+        for (len in 1..n/2) {
+            if (n % len == 0) {
+                val pattern = s.substring(0, len)
+                val repeats = n / len
+
+                if (pattern.repeat(repeats) == s) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
 }
 
 fun main() {
     println(InvalidIdSolver.answerPartOne(File(InvalidIdSolver.filePath)))
+    println(InvalidIdSolver.answerPartTwo(File(InvalidIdSolver.filePath)))
 }
