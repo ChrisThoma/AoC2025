1497550f2acbc53c3950109036040f4b094420a7
chris thoma <christhoma0@gmail.com>
2025-12-02 15:09:24 -0500

    Solves day 2 part 1.

---
+++ src/main/kotlin/day2/InvalidIdSolver.kt
@@ -0,0 +1,50 @@
+package day2
+
+import ProblemRunner
+import java.io.File
+import java.util.*
+
+object InvalidIdSolver : ProblemRunner {
+    override val filePath: String
+        get() = "src/main/kotlin/day2/input.txt"
+
+    override fun answerPartOne(input: File): String {
+        val scanner = Scanner(input)
+        val ranges = scanner.nextLine().split(',')
+        var total: Long = 0L
+        for (range in ranges) {
+            val pair = range.split('-')
+            val low = pair[0]
+            val high = pair[1]
+            val distance = high.toLong() - low.toLong()
+            for (i in 0..distance) {
+                val current = low.toLong() + i
+                val currentString = current.toString()
+                val currentLength = current.toString().length
+                var repeatPos = currentLength / 2
+                var isInvalid = true
+                if (currentLength % 2 != 0) { continue }
+                for (j in 0..<currentLength / 2) {
+                    var k = repeatPos
+                    if (currentString[j] != currentString[k]) {
+                        isInvalid = false
+                        break
+                    }
+                    repeatPos++
+                }
+                if (isInvalid) {
+                    total += current
+                }
+            }
+        }
+        return "$total"
+    }
+
+    override fun answerPartTwo(input: File): String {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main() {
+    println(InvalidIdSolver.answerPartOne(File(InvalidIdSolver.filePath)))
+}
