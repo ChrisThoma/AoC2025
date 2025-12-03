8ec589d46dc9c25fb081f513390734a3a3810b74
chris thoma <christhoma0@gmail.com>
2025-12-03 09:50:40 -0500

    Solves day 3 part 2.

--- src/main/kotlin/day3/JoltageSolver.kt
+++ src/main/kotlin/day3/JoltageSolver.kt
@@ -2,6 +2,7 @@ package day3
 
 import ProblemRunner
 import java.io.File
+import java.lang.StringBuilder
 import java.util.*
 import kotlin.math.max
 
@@ -17,22 +18,43 @@ object JoltageSolver : ProblemRunner {
             var bankMax = 0
             for (i in bank.indices) {
                 val tensDigit = bank[i]
-                for (j in i+1..<bank.length) {
+                for (j in i + 1..<bank.length) {
                     val onesDigit = bank[j]
                     val temp = "$tensDigit$onesDigit".toInt()
                     bankMax = max(bankMax, temp)
                 }
             }
-            total+=bankMax
+            total += bankMax
         }
         return "$total"
     }
 
     override fun answerPartTwo(input: File): String {
-        TODO("Not yet implemented")
+        val scanner = Scanner(input)
+        var total = 0L
+        while (scanner.hasNextLine()) {
+            val bank = scanner.nextLine()
+            val joltageStringBuilder = StringBuilder()
+            var lastPosition = -1
+            for (i in 0..11) {
+                var currentDigitMax = 0
+                val digitsLeft = 12 - i
+                for (j in lastPosition+1..bank.length - digitsLeft) {
+                    val potentialDigit = "${bank[j]}".toInt()
+                    if (potentialDigit > currentDigitMax) {
+                        lastPosition = j
+                        currentDigitMax = potentialDigit
+                    }
+                }
+                joltageStringBuilder.append(currentDigitMax)
+            }
+            total += joltageStringBuilder.toString().toLong()
+        }
+        return "$total"
     }
 }
 
 fun main() {
     println(JoltageSolver.answerPartOne(File(JoltageSolver.filePath)))
+    println(JoltageSolver.answerPartTwo(File(JoltageSolver.filePath)))
 }
